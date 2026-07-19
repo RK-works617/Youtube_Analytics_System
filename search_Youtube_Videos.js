6ef2d8a097a22bbf9c4d8e2122c94b7883ca2d89
RK-works617 <ryosuke.koide0327@gmail.com>
2026-07-19 16:01:02 +0900

    Update search_Youtube_Videos.js

--- search_Youtube_Videos.js
+++ search_Youtube_Videos.js
@@ -1,13 +1,12 @@
 function searchYoutubeVideos(){
-  let sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("シート1");
   let apiKey = PropertiesService.getScriptProperties().getProperty("YOUTUBE_DATA_API_KEY");//APIキーを取得
   let query = 'ft重音テト';//検索キーワードを指定
-  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&type=video&maxResults=50&key=${apiKey}`;//５０件の検索結果を取得
+  let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(query)}&type=video&maxResults=50&key=${apiKey}`;
   let response = UrlFetchApp.fetch(url);
   let data = JSON.parse(response.getContentText());
   let last = sheet.getLastRow()+1;
-  //５０件の動画データを取得して、スプレッドシートに転記
-  for (i=0 ; i<data.items.length ; i++){
+  for (let i=0 ; i<data.items.length ; i++){
     let title = data.items[i].snippet.title;
     let channel = data.items[i].snippet.channelTitle;
     let release = data.items[i].snippet.publishedAt;
